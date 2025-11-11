4498272d370bdf76768c36f7ae1c42867d88a575
Aashina Saxena <aashinasaxena14@gmail.com>
2025-11-11 19:27:24 +0530

    deployment changes

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,7 @@
 //   return `/assets/${path}`;
 // };
 
-export const getImageUrl = (path) => {
-  return new URL(`/assets/${path}`, import.meta.url).href;
-};
+// export const getImageUrl = (path) => {
+//   return new URL(`/assets/${path}`, import.meta.url).href;
+// };
+export const getImageUrl = (path) => `/assets/${path}`;
